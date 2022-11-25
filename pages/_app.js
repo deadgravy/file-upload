ebfebd529efa34f3fb55f9adba0bd402198ed283
Adeeb <adityaadeeb04@gmail.com>
2022-11-25 10:17:35 +0800

    file upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { useState } from 'react';
+import { SessionContextProvider, Session } from '@supabase/auth-helpers-react';
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
+import { QueryClientProvider, QueryClient } from 'react-query';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }) => {
+  const [supabaseClient] = useState(() => createBrowserSupabaseClient());
+  const [queryClient] = useState(() => new QueryClient());
 
-export default MyApp
+  const getLayout = Component.getLayout || ((page) => page);
+
+  return (
+    <SessionContextProvider
+      supabaseClient={supabaseClient}
+      initialSession={pageProps.initialSession}
+    >
+      <QueryClientProvider client={queryClient}>
+        {getLayout(<Component {...pageProps} />)}
+      </QueryClientProvider>
+    </SessionContextProvider>
+  );
+};
+
+export default MyApp;
